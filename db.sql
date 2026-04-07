ccdf13cb4c6d8526cb072b295cb6d62959b38981
farahar2 <farahafouzar@gmail.com>
2026-04-07 12:56:16 +0200

    ajouter script database

---
+++ db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE MaraFood CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE MaraFood;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    prep_time INT NOT NULL,
+    cook_time INT DEFAULT 0,
+    portions INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    user_id INT NOT NULL,
+    category_id INT DEFAULT NULL,
+    
+    CONSTRAINT fk_recipe_user 
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_recipe_category 
+        FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL,
+    
+    INDEX idx_user_id (user_id),
+    INDEX idx_category_id (category_id)
+);
