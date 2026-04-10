ea4c115131338201c66a291071b067f981b7d8e7
farahar2 <farahafouzar@gmail.com>
2026-04-10 13:52:32 +0200

    modification de index

--- index.php
+++ index.php
@@ -21,7 +21,185 @@ switch ($action) {
     
     case 'categories_create':
         require_once 'controllers/CategoryController.php';
-        (new CategoryController())->create();
+        (new CategoryController())->create();        <?php
+        <?php
+        session_start();
+        
+        // Sanitize action
+        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? 'home';
+        
+        switch ($action) {
+            // Accueil
+            case 'home':
+                require __DIR__ . '/views/home.php';
+                break;
+            
+            // Catégories
+            case 'categories':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->index();
+                break;
+            
+            case 'categories_create':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->create();
+                break;
+            
+            case 'categories_store':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->store();
+                break;
+            
+            case 'categories_edit':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->edit();
+                break;
+            
+            case 'categories_update':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->update();
+                break;
+            
+            case 'categories_delete':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->delete();
+                break;
+            
+            // Recettes
+            case 'recipes':
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->index();
+                break;
+            
+            case 'recipes_show':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->show($id);
+                break;
+            
+            case 'recipes_create':
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->create();
+                break;
+            
+            case 'recipes_edit':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->edit($id);
+                break;
+            
+            case 'recipes_delete':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->delete($id);
+                break;
+            
+            default:
+                http_response_code(404);
+                echo "404 - Page introuvable";
+                break;
+        }        <?php
+        <?php
+        session_start();
+        
+        // Sanitize action
+        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? 'home';
+        
+        switch ($action) {
+            // Accueil
+            case 'home':
+                require __DIR__ . '/views/home.php';
+                break;
+            
+            // Catégories
+            case 'categories':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->index();
+                break;
+            
+            case 'categories_create':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->create();
+                break;
+            
+            case 'categories_store':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->store();
+                break;
+            
+            case 'categories_edit':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->edit();
+                break;
+            
+            case 'categories_update':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->update();
+                break;
+            
+            case 'categories_delete':
+                require_once 'controllers/CategoryController.php';
+                (new CategoryController())->delete();
+                break;
+            
+            // Recettes
+            case 'recipes':
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->index();
+                break;
+            
+            case 'recipes_show':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->show($id);
+                break;
+            
+            case 'recipes_create':
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->create();
+                break;
+            
+            case 'recipes_edit':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->edit($id);
+                break;
+            
+            case 'recipes_delete':
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                if (!$id) {
+                    header('Location: index.php?action=recipes');
+                    exit;
+                }
+                require_once 'controllers/RecipeController.php';
+                (new RecipeController())->delete($id);
+                break;
+            
+            default:
+                http_response_code(404);
+                echo "404 - Page introuvable";
+                break;
+        }
         break;
     
     case 'categories_store':
