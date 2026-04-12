36d3f75ac4998519e6d894bf51010c7522bb38c5
Dev-Lhabib <elhabibmahfoud@gmail.com>
2026-04-12 04:36:43 -0700

    fix: protect routes with authentication

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+function requireAuth()
+{
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: index.php?action=login');
+        exit;
+    }
+}
+
 // Sanitize action
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? 'home';
 
@@ -26,42 +34,50 @@ switch ($action) {
 
     // Catégories
     case 'categories':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->index();
         break;
     
     case 'categories_create':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->create();
         break;
     
     case 'categories_store':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->store();
         break;
     
     case 'categories_edit':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->edit();
         break;
     
     case 'categories_update':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->update();
         break;
     
     case 'categories_delete':
+        requireAuth();
         require_once 'controllers/CategoryController.php';
         (new CategoryController())->delete();
         break;
     
     // Recettes
     case 'recipes':
+        requireAuth();
         require_once 'controllers/RecipeController.php';
         (new RecipeController())->index();
         break;
     
     case 'recipes_show':
+        requireAuth();
         $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
         if (!$id) {
             header('Location: index.php?action=recipes');
@@ -72,11 +88,13 @@ switch ($action) {
         break;
     
     case 'recipes_create':
+        requireAuth();
         require_once 'controllers/RecipeController.php';
         (new RecipeController())->create();
         break;
     
     case 'recipes_edit':
+        requireAuth();
         $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
         if (!$id) {
             header('Location: index.php?action=recipes');
@@ -87,6 +105,7 @@ switch ($action) {
         break;
     
     case 'recipes_delete':
+        requireAuth();
         $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
         if (!$id) {
             header('Location: index.php?action=recipes');
